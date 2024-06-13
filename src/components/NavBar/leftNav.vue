0a77db9c8dc8eccc5234c64816258827d302dcc4
TaufikM3 <341.mth@gmail.com>
2024-06-13 21:13:18 +0700

    Penambahan tooltip, ketika cursor mengarah kepada icon navigasi

---
+++ src/components/NavBar/leftNav.vue
@@ -0,0 +1,62 @@
+<template>
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="rail = true"
+      >
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          nav
+        >
+          <template v-slot:append>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-account"  value="/Buku" color="primary">
+            <v-tooltip
+            activator="parent"
+            location="end">
+            Home</v-tooltip>
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline"  value="Kategori">
+            <v-tooltip
+            activator="parent"
+            location="end">
+            Home</v-tooltip>
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-account"  value="Pengguna">
+            <v-tooltip
+            activator="parent"
+            location="end">
+            Home</v-tooltip>
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-account" value="Pesanan">
+            <v-tooltip
+            activator="parent"
+            location="end">
+            Home</v-tooltip>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px"></v-main>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true,
+        rail: true,
+      }
+    },
+  }
+</script>
